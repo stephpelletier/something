68683b24f020bc50d04186d295b5d023f44d2520
Stephane Pelletier-Lamothe <stephane.pelletierlamothe@gmail.com>
2016-08-10 22:38:01 -0400

    Adding tests to verify the creation of the different character class.

--- src/test/java/com/stephpelletier/dnd/character/CharacterFactoryTest.java
+++ src/test/java/com/stephpelletier/dnd/character/CharacterFactoryTest.java
@@ -15,10 +15,74 @@ public class CharacterFactoryTest  {
     }
 
     @Test
+    public void givenABarbarianInputWhenCreatingCharacterThenOutputABarbarianClass() throws Exception {
+        Character barbarian = characterFactory.createCharacter("Barbarian");
+        assertTrue(barbarian instanceof Barbarian);
+    }
+
+    @Test
+    public void givenABardInputWhenCreatingCharacterThenOutputABardClass() throws Exception {
+        Character bard = characterFactory.createCharacter("Bard");
+        assertTrue(bard instanceof Bard);
+    }
+
+    @Test
     public void givenAClericInputWhenCreatingCharacterThenOutputAClericClass() throws Exception {
         Character cleric = characterFactory.createCharacter("Cleric");
         assertTrue(cleric instanceof Cleric);
     }
 
+    @Test
+    public void givenADruidInputWhenCreatingCharacterThenOutputADruidClass() throws Exception {
+        Character druid = characterFactory.createCharacter("Druid");
+        assertTrue(druid instanceof Druid);
+    }
 
+    @Test
+    public void givenAFighterInputWhenCreatingCharacterThenOutputAFighterClass() throws Exception {
+        Character fighter = characterFactory.createCharacter("Fighter");
+        assertTrue(fighter instanceof Fighter);
+    }
+
+    @Test
+    public void givenAMonkInputWhenCreatingCharacterThenOutputAMonkClass() throws Exception {
+        Character monk = characterFactory.createCharacter("Monk");
+        assertTrue(monk instanceof Monk);
+    }
+
+    @Test
+    public void givenAPaladinInputWhenCreatingCharacterThenOutputAPaladinClass() throws Exception {
+        Character paladin = characterFactory.createCharacter("Paladin");
+        assertTrue(paladin instanceof Paladin);
+    }
+
+    @Test
+    public void givenARangerInputWhenCreatingCharacterThenOutputARangerClass() throws Exception {
+        Character ranger = characterFactory.createCharacter("Ranger");
+        assertTrue(ranger instanceof Ranger);
+    }
+
+    @Test
+    public void givenARogueInputWhenCreatingCharacterThenOutputARogueClass() throws Exception {
+        Character rogue = characterFactory.createCharacter("Rogue");
+        assertTrue(rogue instanceof Rogue);
+    }
+
+    @Test
+    public void givenASorcererInputWhenCreatingCharacterThenOutputASorcererClass() throws Exception {
+        Character sorcerer = characterFactory.createCharacter("Sorcerer");
+        assertTrue(sorcerer instanceof Sorcerer);
+    }
+
+    @Test
+    public void givenAWarlockInputWhenCreatingCharacterThenOutputAWarlockClass() throws Exception {
+        Character warlock = characterFactory.createCharacter("Warlock");
+        assertTrue(warlock instanceof Warlock);
+    }
+
+    @Test
+    public void givenAWizardInputWhenCreatingCharacterThenOutputAWizardClass() throws Exception {
+        Character wizard = characterFactory.createCharacter("Wizard");
+        assertTrue(wizard instanceof Wizard);
+    }
 }
